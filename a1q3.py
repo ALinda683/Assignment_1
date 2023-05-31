27462bdea050974f18799d12192f14a509cfb405
ALinda683 <ugj683@usask.ca>
2023-05-30 20:14:21 -0600

    Initial commit: Function initialRead_state added and docstring added successfully. Input Files are also added to verify the program when done.

---
+++ a1q3.py
@@ -0,0 +1,14 @@
+# Name: Alinda Kumar Mazumder
+# NSID: ugj683
+# Student ID: 11342454
+# Course: CMPT 145
+
+def initialRead_state(filename):
+    """ Reads a file's contents and returns the starting state as a list of lists.
+           Args: filename (str): The name of the file to read.
+           Returns: list: A collection of lists representing the initial state. Each row of the initial state is represented by an inner list.
+       with open("input_1.txt", 'r') as file:
+           lines = file.readlines()
+       initialState = [list(line.strip()) for line in lines]
+       return initialState
+       """
